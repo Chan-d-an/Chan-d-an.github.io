73b332b34e4b726ec97ed78d0ce42c6afd195fd3
ck5172857@gmail.com <132717800+Chan-d-an@users.noreply.github.com>
2025-06-21 21:09:39 +0530

    add progressive web app functinality

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/CHAN-D-AN.GITHUB.IO/',
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Chandan PWA',
+        short_name: 'Chandan',
+        start_url: '/CHAN-D-AN.GITHUB.IO/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: '/CHAN-D-AN.GITHUB.IO/vite.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml'
+          }
+        ]
+      }
+    })
+  ]
 })
